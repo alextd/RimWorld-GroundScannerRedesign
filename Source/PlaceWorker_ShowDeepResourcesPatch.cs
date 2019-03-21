c79c6020c218a212a29eb8c92d059c63b4cca6de
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-21 03:44:15 -0700

    Show resource grid when placing drills - whether or not you have a scanner or it's on, since now the grid is hidden until scanned, and what is scanned should easily be remembered

---
+++ Source/PlaceWorker_ShowDeepResourcesPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Ground_Scanner_Redesign
+{
+	[HarmonyPatch(typeof(PlaceWorker_ShowDeepResources), nameof(PlaceWorker_ShowDeepResources.DrawGhost))]
+	class PlaceWorker_ShowDeepResourcesPatch
+	{
+		//public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol)
+		public static bool Prefix()
+		{
+			//Simply show what we know. Don't need the scanner on to remember what it scanned.
+			Map currentMap = Find.CurrentMap;
+			currentMap.deepResourceGrid.MarkForDraw();
+			return false;
+		}
+	}
+}
