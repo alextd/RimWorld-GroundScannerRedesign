6b71419cfed2afb48ba003f2e901adc846a52169
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-21 04:00:20 -0700

    Draw known deep resources when scanner is selected, powered on/off no longer matters.

---
+++ Source/CompDeepScannerPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Ground_Scanner_Redesign
+{
+	[HarmonyPatch(typeof(CompDeepScanner), nameof(CompDeepScanner.PostDrawExtraSelectionOverlays))]
+	class CompDeepScannerPatch
+	{
+		//public override void PostDrawExtraSelectionOverlays()
+		public static bool Prefix(CompDeepScanner __instance)
+		{
+			//no need to check powerComp.PowerOn, just draw what was previously scanned.
+			__instance.parent.Map.deepResourceGrid.MarkForDraw();
+			return false;
+		}
+	}
+}
